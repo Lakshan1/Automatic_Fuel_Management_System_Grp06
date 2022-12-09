718ecbb2939a29d875dc04e204308cb2df12a463
Shanthakumar Lakshan <70530770+Lakshan1@users.noreply.github.com>
2022-12-09 05:19:11 +0000

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ def task():
     requests.get("https://automatic-fuel-management-system.onrender.com/api/refreshQuota/")
 
 
-schedule.every().monday.at("00.00").do(task)
+schedule.every().monday.at("12.01").do(task)
 
 while True:
     schedule.run_pending()
